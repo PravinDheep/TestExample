7b53a32796beb6cd709cde673c8604eac60d6adb
PravinDheep <pradeepharidass@gmail.com>
2022-02-04 12:40:55 +0530

    Adding for loop

--- src/com/workouts/TestExample.java
+++ src/com/workouts/TestExample.java
@@ -4,6 +4,9 @@ public class TestExample {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		for(int i=1; i <= 10; i++) {
+			System.out.println(" Index: " + i);
+		}
 	}
 
 }
